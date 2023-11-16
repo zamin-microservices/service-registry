515f3619604a696fd22dcfe2a6a55684ba5342f1
zamss <aqeel.zamin@gmail.com>
2023-11-16 08:18:10 +0530

    service registery pushed to git

---
+++ src/test/java/com/zamincode/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zamincode.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
